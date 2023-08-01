5bc008c4f331024c2e346fef4205e0eae4714c29
bridgetcarlile <104595104+bridgetcarlile@users.noreply.github.com>
2023-07-31 19:00:28 -0500

    changed wording again to match autograder

--- scriptHelper.js
+++ scriptHelper.js
@@ -62,26 +62,26 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 
      } else {
 
-        pilotStatus.innerHTML = `Pilot ${pilot} is ready.`;
-        copilotStatus.innerHTML = `Co-pilot ${copilot} is ready.`
+        pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+        copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
         list.style.visibility = "visible";
 
         if (fuelLevel <10000 && cargoLevel >10000) {
         fuelStatus.innerHTML = "Fuel level too low for launch";
         cargoStatus.innerHTML = "Cargo mass too heavy for launch";
-        launchStatus.innerHTML = "Shuttle is Not Ready for Launch";
+        launchStatus.innerHTML = "Shuttle Not Ready for Launch";
         launchStatus.style.color = "#C7254E";
 
     } else if (fuelLevel >= 10000 && cargoLevel >10000) {
         fuelStatus.innerHTML = "Fuel level high enough for launch";
         cargoStatus.innerHTML = "Cargo mass too heavy for launch";
-        launchStatus.innerHTML = "Shuttle is Not Ready for Launch";
+        launchStatus.innerHTML = "Shuttle Not Ready for Launch";
         launchStatus.style.color = "#C7254E";
     
     } else if (fuelLevel <10000 && cargoLevel <=10000) {
         fuelStatus.innerHTML = "Fuel level too low for launch";
         cargoStatus.innerHTML = "Cargo mass low enough for launch"
-        launchStatus.innerHTML = "Shuttle is Not Ready for Launch";
+        launchStatus.innerHTML = "Shuttle Not Ready for Launch";
         launchStatus.style.color = "#C7254E";
     } else {
         launchStatus.style.color = "#419F6A";
